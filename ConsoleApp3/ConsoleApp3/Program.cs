54c73facad58eb62947cadece9a6bec06f6303e3
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2020-12-24 14:56:34 +0200

    Doroblena

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            ;
+
+
+            char[] liter = { 'S', 'Y', 'M', 'B', 'O', 'L' };
+            string bukv = new string(liter);
+            Console.WriteLine(bukv);
+            string string1 = bukv;
+            int length;
+            length = string1.Length;  
+            Rydok ryadok = new Rydok(string1);
+           
+            Console.WriteLine(ryadok.ToString());
+
+            Console.WriteLine("лiтер - " + length);
+            ryadok.Reverse();
+            Console.WriteLine("перевернутий рядок");
+            Console.WriteLine(ryadok.ToString());
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
